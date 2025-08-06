2036a998e00647f702579f9c1fcece5b03f4924d
Nabila Ayaba <nabilaayaba@gmail.com>
2025-08-06 14:15:08 +0300

    Sign in almost finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import SignUp from './components/SignUp/SignUp'
+import SignIn from './components/SignIn/SignIn'
 import { Route, Routes } from 'react-router-dom'
 import * as authService from './services/authService.js'
 import { useState } from 'react'
 
 const App = () => {
 
-  const [user, setUser] = useState(null)
+  const initialState = authService.getUser()
+
+  const [user, setUser] = useState(initialState)
 
   const handleSignUp = async (formData) => {
    const res = await authService.signUp(formData)
    setUser(res)
   }
 
-    const handleSignOut = () => {
+  const handleSignOut = () => {
     localStorage.removeItem('token')
     setUser(null)
+  }
+
+  const handleSignIn = async (formData) => {
+    const res = await authService.signIn(formData)
+    setUser(res)
   }
 
   return (
@@ -25,8 +33,9 @@ const App = () => {
       <Routes>
           <Route path='/' element={<h1>Hello world!</h1>} />
           <Route path='/sign-up' element={<SignUp handleSignUp={handleSignUp} />} />
+          <Route path='/sign-in' element={<SignIn handleSignIn={handleSignIn} />} />
           <Route path='*' element={<h1>404</h1>} />
-      </Routes>
+    </Routes>
     </>
   )
 }
