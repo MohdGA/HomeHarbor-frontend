12654c8561ded8a24bfa137c9bbde989b178c3f7
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-08-29 21:45:20 +0300

    Add Properties List

---
+++ src/components/PropertyList/PropertyList.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+const PropertyList = (props) => {
+  const { properties } = props; 
+
+  if (!properties || properties.length === 0) {
+    return <h2>No Properties Yet</h2>;
+  }
+
+  return (
+    <div>
+      <h1>Properties List</h1>
+         {properties.map((property, index) => (
+            <div key={property._id || index}>
+              
+            <Link to={`/properties/${property._id}`}>
+
+                    <h2>{property.title}</h2>
+         </Link>
+                  <p>Price: {property.price} BHD</p>
+                  <p>Rooms: {property.numOfRooms}     </p>
+                  <p>Bathrooms: {property.numOfBathrooms}</p> 
+                  <p>Location: {property.location}</p>
+
+                    <p><small> Owner: {property.user?.username || "Unknown"} </small></p>
+             <hr />
+             
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PropertyList;
