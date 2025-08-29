194ce31c79f875bf7de3ea957431dd91e5f131dd
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-08-29 18:02:31 +0300

    Update and fixing

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,63 +1,59 @@
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/auth`
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}`
 
 const signUp = async (formData) => {
+
   try {
-    const res = await fetch(`${BASE_URL}/sign-up`, {
+    const res = await fetch(`${BASE_URL}/register`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(formData)
     })
+
     const data = await res.json()
 
-    // Checking if the response code was "ok"
-    if (!res.ok) throw new Error(data.err || 'Something went wrong')
+    if (!res.ok) throw new Error(data.detail || 'Something went wrong')
 
-    if (data.token) {
-      localStorage.setItem('token', data.token)
-      const decodedToken = JSON.parse(atob(data.token.split('.')[1]))
-      return decodedToken
-    }
-
+    return data
   } catch (err) {
-    // throwing an error instead of console.logging
     throw err
   }
 }
 
 const signIn = async (formData) => {
   try {
-    const res = await fetch(`${BASE_URL}/sign-in`, {
+    const res = await fetch(`${BASE_URL}/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(formData)
     })
+
     const data = await res.json()
+
+    if (!res.ok) throw new Error(data.detail || 'Something went wrong')
+
     if (data.token) {
-      // save the token in local storage
       localStorage.setItem('token', data.token)
-      // returning the user info to use in our app
       const decodedToken = JSON.parse(atob(data.token.split('.')[1]))
       return decodedToken
     }
 
   } catch (err) {
-    console.log(err)
+    throw err
   }
 }
+
 
 const getUser = () => {
   const token = localStorage.getItem('token')
   if (token) {
-    // return decoded token with user object
-      const decodedToken = JSON.parse(atob(token.split('.')[1]))
-      return decodedToken
-  } else {
-    return null
-  }
+    const decodedToken = JSON.parse(atob(token.split('.')[1]))
+    return decodedToken
+  } 
+  return null
 }
 
 export {
