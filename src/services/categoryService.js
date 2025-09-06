77b1b630ded277f04c6ad5c2dc26eb5fbdd6ca47
MohdGA <generalassemblyse@gmail.com>
2025-09-06 19:17:40 +0300

    test deployment2

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,7 +1,7 @@
 const BASE_URL = import.meta.env.VITE_BACK_END_SERVER_URL.replace(/\/$/, '')
 
 async function getCategories() {
-  const res = await fetch(`${BASE}/categories`);
+  const res = await fetch(`${BASE_URL}/categories`);
   if (!res.ok) {
     const err = await res.json().catch(() => ({}));
     throw new Error(err.detail || "Failed to load categories");
